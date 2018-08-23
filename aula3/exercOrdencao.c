665a491e288b687f6a759a6ec99149332e6d2ca9
RAMON NASCIMENTO <UC14100797@SLAB.UCB.BR>
2018-08-23 11:38:57 -0300

    update

--- aula3/exercOrdencao.c
+++ aula3/exercOrdencao.c
@@ -5,57 +5,81 @@
 #define MAX_VETOR 100
 
 void bubbleSort(int *vNumbers, int amountElements);
+void selectionSort(int *vNumbers, int amountElements);
+void insertionSort(int *vNumbers, int amountElements);
 
 int main() {
 //Declarações
 	int vNumbers1[MAX_VETOR] = {0}, vNumbers2[MAX_VETOR] = {0}, vNumbers3[MAX_VETOR] = {0};
 	int countAmount;
-//Inicializações 
+//Inicializações
 	srand(time(NULL));
 //Instruções
 	for(countAmount = 0; countAmount < MAX_VETOR; countAmount++) {
 		vNumbers1[countAmount] = rand() % MAX_VETOR;
 	}
-	
+
 	memcpy(vNumbers2, vNumbers1, sizeof(vNumbers1));
 	memcpy(vNumbers3, vNumbers1, sizeof(vNumbers1));
 
 	bubbleSort(vNumbers1, MAX_VETOR);
+	selectionSort(vNumbers2, MAX_VETOR);
+	insertionSort(vNumbers3, MAX_VETOR);
+	
+	
+	printf("+--------+-----------+-----------+\n");
+	printf("| Bubble | Selection | Insertion |\n");
+	printf("+--------+-----------+-----------+\n");
+	for(countAmount = 0; countAmount < MAX_VETOR; countAmount++) {
+		printf("| %-6.3i | %-9.3i | %-9.3i |\n", vNumbers1[countAmount], vNumbers2[countAmount], vNumbers3[countAmount]);
+	}
+	printf("+--------+-----------+-----------+");
 
-	for(countAmount = 0; countAmount < MAX_VETOR; countAmount++){
-		printf("%3.i\n", vNumbers1[countAmount]);
-	}
-	
-	/*printf("+---------+--------+\n");
-	printf("| xNumber | Number |\n");
-	printf("+---------+--------+\n");
-	for(countAmount = 0; countAmount < MAX_VETOR; countAmount++){
-		printf("|%-9.1i|%-8.3i|\n", countAmount, vNumbers1[countAmount]);
-	}
-	printf("+---------+--------+");
-	
-	printf("\n\n+---------+--------+\n");
-	printf("| xNumber | Number |\n");
-	printf("+---------+--------+\n");
-	for(countAmount = 0; countAmount < MAX_VETOR; countAmount++){
-		printf("|%-9.1i|%-8.3i|\n", countAmount, vNumbers2[countAmount]);
-	}
-	printf("+---------+--------+");*/
-	
 	return 0;
 }
 
-void bubbleSort(int *vNumbers, int amountElements){
+void bubbleSort(int *vNumbers, int amountElements) {
 //Declarações
-	int countAmount, countAux, tempVector;
+	int countAmount, countAux, tempNumber;
 //Instruções
-	for(countAmount = 0; countAmount < amountElements; countAmount++){
-		for(countAux = 1; countAux < amountElements; countAux++){
-			if(vNumbers[countAux - 1] > vNumbers[countAux]){
-				tempVector = vNumbers[countAux - 1];
+	for(countAmount = 0; countAmount < amountElements; countAmount++) {
+		for(countAux = 1; countAux < amountElements; countAux++) {
+			if(vNumbers[countAux - 1] > vNumbers[countAux]) {
+				tempNumber = vNumbers[countAux - 1];
 				vNumbers[countAux - 1] = vNumbers[countAux];
-				vNumbers[countAux] = tempVector;
+				vNumbers[countAux] = tempNumber;
 			}
 		}
+	}
+}
+
+void selectionSort(int *vNumbers, int amountElements){
+//Declarações
+	int countAmount, countAux, min, tempNumber;
+//Instruções
+	for(countAmount = 0; countAmount < amountElements -1 ; countAmount++) {
+		min = countAmount;
+		for(countAux = countAmount + 1; countAux < amountElements; countAux++) {
+			if(vNumbers[countAux] < vNumbers[min]) {
+				min = countAux;
+			}
+		}
+		tempNumber = vNumbers[countAmount];
+		vNumbers[countAmount] = vNumbers[min];
+		vNumbers[min] = tempNumber;
+	}
+}
+
+void insertionSort(int *vNumbers, int amountElements){
+//Declarações
+	int countAmount, countAux, tempNumber;
+//Instruções
+	for(countAux = 1; countAux < amountElements; countAux++) {
+		tempNumber = vNumbers[countAux];
+		countAmount = countAux - 1;
+		while((countAmount >= 0) && (vNumbers[countAmount] > tempNumber)){
+			vNumbers[countAmount + 1] = vNumbers[countAmount--];
+		}
+		vNumbers[countAmount + 1] = tempNumber;
 	}
 }
